257257a97f391956b69bfeb51d0819fdaa59f40c
AnhLe <quyanhle83@gmail.com>
2022-10-27 16:13:37 -0700

    Time: 504 ms (13.97%), Space: 98.6 MB (8.01%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        
+        for (int l=0, h=s1.length(); h<=s2.length(); l++, h++) {
+            if (isPermutation(s1, s2.substring(l, h))) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean isPermutation(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        
+        int[] map1 = new int[128];
+        int[] map2 = new int[128];
+        
+        for (int i=0, j=0; i< s1.length(); i++, j++) {
+            map1[s1.charAt(i)]++;
+            map2[s2.charAt(j)]++;
+        }
+        
+        for (int i=0; i<map1.length; i++) {
+            if (map1[i] != map2[i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
