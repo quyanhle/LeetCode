1538afd1b8da701f3e9cb628f7f2487ebf4d5ad8
AnhLe <quyanhle83@gmail.com>
2022-10-28 19:00:34 -0700

    Time: 3 ms (88.42%), Space: 47.3 MB (50.84%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -2,28 +2,27 @@ class Solution {
 
     public int maxAreaOfIsland(int[][] grid) {
         int maxArea = 0;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
         for (int i=0; i< grid.length; i++) {
             for (int j=0; j<grid[i].length; j++) {
-                if (!visited[i][j] && grid[i][j] == 1) {
-                    maxArea = Math.max(maxArea, dfs(grid, visited, i,j, 0));
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, dfs(grid, i,j, 0));
                 }
             }
         }
         return maxArea;
     }
     
-    public int dfs(int[][] idx, boolean[][] visited, int sr, int sc, int area) {
+    public int dfs(int[][] idx, int sr, int sc, int area) {
         int rows = idx.length, cols = idx[0].length;
-        if (sr < 0 || sc >= cols || sr >= rows || sc < 0 || idx[sr][sc] == 0 || visited[sr][sc]) {
+        if (sr < 0 || sc >= cols || sr >= rows || sc < 0 || idx[sr][sc] == 0) {
             return area;
         }
-        visited[sr][sc] = true;
+        idx[sr][sc] = 0;
         area++;
-        area = dfs(idx, visited, sr-1, sc, area);
-        area = dfs(idx, visited,sr, sc+1, area);
-        area = dfs(idx, visited,sr+1, sc, area);
-        area = dfs(idx, visited,sr, sc-1, area);
+        area = dfs(idx, sr-1, sc, area);
+        area = dfs(idx,sr, sc+1, area);
+        area = dfs(idx,sr+1, sc, area);
+        area = dfs(idx,sr, sc-1, area);
         
         return area;
     }
