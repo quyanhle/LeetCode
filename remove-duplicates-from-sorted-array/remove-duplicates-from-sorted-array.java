181498058fa5df2b7dae9ec3b1b7d8acb01a5e8e
AnhLe <quyanhle83@gmail.com>
2022-11-08 19:35:43 -0800

    Time: 2 ms, Memory: 48 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,24 +3,13 @@ class Solution {
         if (nums.length == 1) {
             return 1;
         }
-        // int tobeChanged =1;
-        // for (int i = 1; i<nums.length; i++) {
-        //     if (nums[i] != nums[i-1]) {
-        //         nums[tobeChanged++] = nums[i];
-        //     }
-        // }
-        // return tobeChanged;
-        
-        int swapIdx = 1, curr=1;
-        while (curr<nums.length) {
-            if (nums[curr] != nums[swapIdx-1]) {
-                int temp = nums[swapIdx];
-                nums[swapIdx++] = nums[curr];
-                nums[curr] = temp;
-                
+        int tobeChanged =1;
+        for (int i = 1; i<nums.length; i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[tobeChanged++] = nums[i];
             }
-            curr++;
         }
-        return swapIdx;
+        return tobeChanged;
+        
     }
 }
