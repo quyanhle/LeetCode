343838082263dc82dcbc8f477c42d3f755305f67
AnhLe <quyanhle83@gmail.com>
2022-11-09 21:02:09 -0800

    Time: 1 ms, Memory: 43.8 MB - LeetHub

---
+++ move-zeroes/move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZeroIdx = 0;
+        
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIdx++] = nums[i];
+            }
+        }
+        
+        for (int i=nonZeroIdx; i<nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
