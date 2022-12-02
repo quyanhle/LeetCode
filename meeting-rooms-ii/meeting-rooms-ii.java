16fe5cc6b660f0481f023ca4e78f56f436aafb0c
AnhLe <quyanhle83@gmail.com>
2022-12-02 00:16:39 -0800

    Time: 13 ms, Memory: 45.8 MB - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.java
+++ meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,17 +1,21 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         PriorityQueue<Integer> endHeap = new PriorityQueue<>();
         int rooms = 0;
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        for (int i = 0; i< intervals.length; i++) {
+        for (int i = 0; i < intervals.length; i++) {
+            heap.offer(intervals[i]);
+        }
+        while(!heap.isEmpty()) {
+            int[] interval = heap.poll();
             if (rooms == 0) {
                 rooms++;
-            } else if (intervals[i][0] < endHeap.peek()) {
+            } else if (interval[0] < endHeap.peek()) {
                 rooms++;
             } else {
                 endHeap.poll();
             }
-            endHeap.offer(intervals[i][1]);
+            endHeap.offer(interval[1]);
         }
         
         return rooms;
