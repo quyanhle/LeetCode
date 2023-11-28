64be11c51cb6b48df86debf934901132256b8de5
AnhLe <quyanhle83@gmail.com>
2023-11-27 21:25:15 -0500

    Time: 2 ms (81.23%), Space: 44.7 MB (20.26%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if (nums.length <= 1) {
+            return true;
+        }
+        int jumpIdx = 0;
+        for (int i = 0; i<nums.length && i<= jumpIdx; i++) {
+            jumpIdx = Math.max(i+nums[i], jumpIdx);
+            if (jumpIdx >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
