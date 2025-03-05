307eff30d80f86300b27597d47c6db88c49d721d
AnhLe <quyanhle83@gmail.com>
2025-03-05 14:10:50 -0500

    Time: 40 ms, Memory: 60.3 MB - LeetHub

---
+++ maximum-gap/maximum-gap.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums.length < 2) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int max = Integer.MIN_VALUE;
+        
+        for (int i = 1; i<nums.length; i++) {
+            max = Math.max(max, nums[i] - nums[i-1]);
+        }
+        
+        return max;
+    }
+}
