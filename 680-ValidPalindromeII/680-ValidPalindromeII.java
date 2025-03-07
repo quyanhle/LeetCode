0f183f1d6b731875180a21f6d0a42b1e2cbcdec6
AnhLe <quyanhle83@gmail.com>
2025-03-07 14:56:38 -0500

    Create file

---
+++ 680-ValidPalindromeII/680-ValidPalindromeII.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return helper(s, left+1, right) || helper(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean helper(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            } 
+        }
+
+        return true;
+    }
+}
