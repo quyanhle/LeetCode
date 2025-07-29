d3ed2e939be9c7bf723ff70562b723b4621f4275
AnhLe <quyanhle83@gmail.com>
2025-07-28 22:35:44 -0400

    Time: 2 ms, Memory: 21.9 MB - LeetHub

---
+++ 0two-sum-ii-input-array-is-sorted/0two-sum-ii-input-array-is-sorted.php
@@ -0,0 +1,23 @@
+class Solution {
+
+    /**
+     * @param Integer[] $numbers
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($numbers, $target) {
+        $i = 0;
+        $j = count($numbers) - 1;
+        while ($i < $j) {
+            if ($numbers[$i] + $numbers[$j] == $target) {
+                return [$i+1, $j+1];
+            }
+            
+            elseif ($numbers[$i] + $numbers[$j] < $target) {
+                $i++;
+            } else {
+                $j--;
+            }
+        }
+    }
+}
