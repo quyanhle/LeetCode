478a5a095c74dc42cabc1462863c3e74af4a5115
AnhLe <quyanhle83@gmail.com>
2025-08-07 18:18:09 -0400

    Create 1463-TheKWeakestRowsInAMatrix

---
+++ 1463-TheKWeakestRowsInAMatrix/1463-TheKWeakestRowsInAMatrix.java
@@ -0,0 +1,27 @@
+// Last updated: 8/7/2025, 6:18:09 PM
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int[] result = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<HashMap.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b) ->
+            a.getValue() != b.getValue() ? a.getValue() - b.getValue() : a.getKey() - b.getKey()
+                                                                                 );
+        for(int i = 0; i < mat.length; i++) {
+            int numberOf1 = 0;
+            for (int j=0; j<mat[i].length; j++) {
+                if(mat[i][j] == 1) {
+                    numberOf1++;
+                }
+            }
+            map.put(i, numberOf1);
+        }
+        for (HashMap.Entry<Integer, Integer> e : map.entrySet()) {
+            heap.offer(e);
+        }
+        
+        for (int i = 0; i<k; i++) {
+            result[i] = heap.poll().getKey();
+        }
+        return result;
+    }
+}
