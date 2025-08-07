7bf133cbea72600178764d4bf783627e6f504b8c
AnhLe <quyanhle83@gmail.com>
2025-08-07 18:18:51 -0400

    Create 253-MeetingRoomsIi

---
+++ 253-MeetingRoomsIi/253-MeetingRoomsIi.java
@@ -0,0 +1,24 @@
+// Last updated: 8/7/2025, 6:18:51 PM
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> endHeap = new PriorityQueue<>();
+        int rooms = 0;
+        for (int i = 0; i < intervals.length; i++) {
+            heap.offer(intervals[i]);
+        }
+        while(!heap.isEmpty()) {
+            int[] interval = heap.poll();
+            if (rooms == 0) {
+                rooms++;
+            } else if (interval[0] < endHeap.peek()) {
+                rooms++;
+            } else {
+                endHeap.poll();
+            }
+            endHeap.offer(interval[1]);
+        }
+        
+        return rooms;
+    }
+}
