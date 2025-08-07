18e27959d2bb8086fdb22ceafbdf3d2dfc15f908
AnhLe <quyanhle83@gmail.com>
2025-08-07 18:18:11 -0400

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,13 @@
+// Last updated: 8/7/2025, 6:18:11 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return () => "Hello World";
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
