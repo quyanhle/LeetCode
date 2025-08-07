db71c1b7f19008759a8969ed6c07e0206160aba8
AnhLe <quyanhle83@gmail.com>
2025-08-07 18:18:20 -0400

    Create 1014-KClosestPointsToOrigin

---
+++ 1014-KClosestPointsToOrigin/1014-KClosestPointsToOrigin.java
@@ -0,0 +1,17 @@
+// Last updated: 8/7/2025, 6:18:20 PM
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int[][] res = new int[k][2];
+        Queue<int[]> heap = new PriorityQueue<>((a,b) -> 
+                                                (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]));
+        for (int i = 0; i < points.length; i++) {
+            heap.offer(points[i]);
+        }
+        
+        for( int i = 0; i<k ; i++) {
+            res[i] = heap.poll();
+        }
+        
+        return res;
+    }
+}
