ac62749920c12f591f63e58141af447458a0898a
AnhLe <quyanhle83@gmail.com>
2025-08-07 18:18:33 -0400

    Create 487-MaxConsecutiveOnesIi

---
+++ 487-MaxConsecutiveOnesIi/487-MaxConsecutiveOnesIi.java
@@ -0,0 +1,19 @@
+// Last updated: 8/7/2025, 6:18:34 PM
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int lastZero = -1;
+        int maxSoFar = 0, left = 0;
+        
+        for (int i = 0; i< nums.length; i++) {
+            if (nums[i] == 0) {
+                if (lastZero >= 0) {
+                    left = lastZero + 1;
+                }
+                lastZero = i;
+            }
+            maxSoFar = Math.max(i-left+1, maxSoFar);
+        }
+        
+        return maxSoFar;
+    }
+}
